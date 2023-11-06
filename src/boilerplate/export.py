fa0c15e60a16cb22321ee5742bd13f0f4bffb6b4
Billy Daly <williamdaly422@gmail.com>
2023-11-05 21:23:12 -0500

    [Issue 73] GitHub action export data (#74)
    * refactor: Removes extra code
    
    * feat: Adds cli.py
    
    * feat: Adds GitHub action to export the data
    
    * fix: Failing CI/CD tests

---
+++ src/boilerplate/export.py
@@ -0,0 +1,56 @@
+# pylint: disable=invalid-name, unspecified-encoding
+import json
+import shlex
+import subprocess
+
+
+ISSUE_FILE = "data/issue-data.json"
+SPRINT_FILE = "data/sprint-data.json"
+
+
+def pipe_command_to_file(command: str, output_file: str):
+    """Write the output of a sub-process to a file"""
+    with open(output_file, "w") as f:
+        subprocess.call(shlex.split(command), stdout=f)
+
+
+def export_project_data(owner: str, project: int, output_file: str) -> None:
+    """Exports and writes GitHub project data to a JSON file"""
+    command = (
+        f"gh project item-list {project} --format json --owner {owner} -L 1000"
+    )
+    pipe_command_to_file(command, output_file)
+
+
+def export_issue_data(owner: str, repo: str, output_file: str) -> None:
+    """Exports and writes GitHub issue data to a JSON file"""
+    command = (
+        "gh issue list --json number,createdAt,closedAt "
+        f"-R {owner}/{repo} -L 1000 --state all"
+    )
+    pipe_command_to_file(command, output_file)
+
+
+def export_github_data(owner: str, repo: str, project: str):
+    """Export issues and project data from GitHub"""
+    print("Exporting issue data")
+    export_issue_data(
+        owner=owner,
+        repo=repo,
+        output_file=ISSUE_FILE,
+    )
+    print("Exporting sprint data")
+    export_project_data(
+        owner=owner,
+        project=project,
+        output_file=SPRINT_FILE,
+    )
+
+
+def load_files():
+    """Checks that the json files can be loaded correctly"""
+    with open(ISSUE_FILE, "r") as f:
+        issues = json.loads(f.read())
+    print(f"Total number of issues: {len(issues)}")
+    print("First issue:")
+    print(issues[0])
---
+++ tests/unit_tests/test_sample.py
@@ -0,0 +1,6 @@
+import boilerplate
+
+
+def test_name():
+    """Tests that the boilerplate code was imported correctly"""
+    assert boilerplate.__name__ == "boilerplate"
